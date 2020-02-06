5f28e5fb32fb86fff77ed1a330e731275462131e
Eldaste <oliver.m.wimmer@vanderbilt.edu>
2020-02-06 14:08:00 -0600

    Still setting up

--- bot2.js
+++ bot2.js
@@ -13,8 +13,22 @@ const errorFile="errorfile.log";
 // Set up enviroment
 var executing=false;
 
+retriveChn = function (idno){
+	return client.channels.get(idno);
+}
+
+retrieveUser = function (idno, chn){
+	return chn.members.get(idno);
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+	
+	
+	//var open=retriveChn("668360850750308358");
+
+	//open=retrieveUser('133438424580489216', open);
+	//open.send("Hello");
 });
 
 client.on('message', msg => {
@@ -27,6 +41,6 @@ client.on('message', msg => {
 		
 		return;
 	}// End program space
-)}
+});
 
 client.login(auth.token);
